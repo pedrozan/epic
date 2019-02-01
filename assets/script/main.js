d534776670bb3adb8601417a79f1e3a9d0a0a204
Pedro Henrique <pedro.schleder@gmail.com>
2019-02-01 14:28:55 -0200

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//     $('.roulette').slick({
+//         centerMode: true,
+//         centerPadding: '60px',
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         responsive: [
+//             {
+//                 breakpoint: 768,
+//                 settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 3
+//                 }
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     arrows: false,
+//                     centerMode: true,
+//                     centerPadding: '40px',
+//                     slidesToShow: 1
+//                 }
+//             }
+//         ]
+//     });
+// });
